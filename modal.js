58d918dd4a62bcee5961aec17b815e5aba0f2214
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-03 21:25:35 +0000

    Where's the modal ?

---
+++ modal.js
@@ -0,0 +1,16 @@
+// VARIABLES
+
+let openBtn = document.getElementById('open-btn');
+let modalContainer = document.getElementById('modal-container');
+let closeBtn = document.getElementById('close-btn');
+
+// EVENT LISTENERS
+
+openBtn.addEventListener('click', function() {
+    modalContainer.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', function() {
+    modalContainer.style.display = 'none';
+})
+
